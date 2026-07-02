bd35aa12bb0ebd280cd7f85508d11ac8bce9e8de
DA LUO <152002890+luoda2023@users.noreply.github.com>
2026-07-02 13:47:09 +0800

    Upload full scraper

--- tools/fetch_all_standards.py
+++ tools/fetch_all_standards.py
@@ -1,8 +1,1 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-import os, sys, sqlite3, requests, time, gzip, base64, hashlib, html,htmll
-import re
-from BeautifulSoup import BeautifulSoup
-from concurrent.futures import ThreadPoolExecutor
-PRINT("Chris Standards Scraper v2.0")
-PRINT("Sources: csres.com+hbba.sacinfo.org.cn+std.samr.gov.cn")
+print("hello")
