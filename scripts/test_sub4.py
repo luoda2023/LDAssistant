a392df8d77ac0a85ff389d6bba15a31b724035b4
DA LUO <152002890+luoda2023@users.noreply.github.com>
2026-07-09 18:33:57 +0800

    Backup: test_sub4.py

---
+++ scripts/test_sub4.py
@@ -0,0 +1,7 @@
+import sys
+sys.stdout.reconfigure(encoding='utf-8')
+sys.path.insert(0, r'C:\ZCODE\scripts')
+from collect_csres import get_subcategories
+subs = get_subcategories('A')
+print(f'子类数={len(subs)}')
+for s in subs[:5]: print(f'  {s[1]}: {s[2][:30]}')
