f318fa831e3f533176fe204d120ebec0abd4ac36
ZBot <zbot@example.com>
2026-08-04 22:04:49 +0800

    fix: 移除OpenXml using别名冲突，Paragraph用WPF完整路径

--- LDAssistant/Views/AiChatWindow.xaml.cs
+++ LDAssistant/Views/AiChatWindow.xaml.cs
@@ -7,7 +7,6 @@ using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using DocumentFormat.OpenXml.Wordprocessing;
 using LDAssistant.Services;
 
 namespace LDAssistant.Views
